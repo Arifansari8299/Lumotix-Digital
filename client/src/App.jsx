099d1332d0baafc2899a8c10e5f7e0294fc0fb79
Arif Ansari <arifkmclu@gmail.com>
2025-08-24 19:29:07 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function App(){
+  return(
+    <div className='min-h-screen bg-amber-100 flex items-center justify-center'>
+      <h2 className='text-3xl font-bold'>App</h2>
+    </div>
+  )
+}
+export default App;
